7bf789ea8ba326350b18f336773799ee135571b1
Carr0t-Hub <79178536+Carr0t-Hub@users.noreply.github.com>
2023-10-13 10:26:13 +0800

    Initial Design

---
+++ User-Side/common/header.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en" >
+<head>
+  <meta charset="UTF-8">
+  <title>DA-BAR | Customer Satisfactory Survey</title>
+  <link href="assets/img/logo.png" rel="icon">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
+  <link href="https://cdnjs.cloudflare.com/ajax/libs/meyer-reset/2.0/reset.min.css" rel="stylesheet">
+  <link href='https://cdn.jsdelivr.net/npm/inter-ui@3.11.0/inter.min.css' rel="stylesheet">
+  <link href="assets/css/styles.css" rel="stylesheet">
+  
+
+</head>
+<body>
