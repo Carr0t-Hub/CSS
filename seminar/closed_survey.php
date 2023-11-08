e3d0c0997b185080a7fb08d60c67d6088fcae147
Paul Osorio <106576044+paul-osorio@users.noreply.github.com>
2023-11-08 13:48:09 +0800

    added closed survey design

--- seminar/closed_survey.php
+++ seminar/closed_survey.php
@@ -2,7 +2,31 @@
 include("../common/header.php");
 include("../config/function.php");
 ?>
-This survey is now closed.
+<div class="vh-100 vw-100 d-flex justify-content-center align-items-center bg-light">
+    <div class="">
+        <div class="py-3">
+            <center>
+                <img src="../assets/img/logo.png" height="84px" alt="DA-BAR">
+            </center>
+        </div>
+        <div class="rounded-3 shadow-sm bg-white p-5 px-4">
+            <div class="d-flex justify-content-center">
+                <i class="fas fa-users-slash text-secondary fa-5x"></i>
+            </div>
+            <br>
+            <p class="text-center" style="font-size: 20px">
+                Sorry but the survey is not accepting<br /> responses anymore. <br>
+            </p>
+
+            <div class="">
+                <div class="d-flex justify-content-center">
+                    <a href="https://bar.gov.ph" class="btn btn-primary">Go to BAR Website</a>
+                </div>
+            </div>
+
+        </div>
+    </div>
+</div>
 <?php
 include("../common/footer.php");
 ?>
